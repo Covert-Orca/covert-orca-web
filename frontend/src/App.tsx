a2902a25e4aa6f879e71cbc2b5b7f6b1033dc77e
Pan885 <59109935+Pan885@users.noreply.github.com>
2021-03-14 23:11:26 +0800

    navigation feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
+import Product from './Products/Products';
+import Home from './Home/Home';
 
 function App() {
   const openMenu = () => {
@@ -11,81 +14,54 @@ function App() {
   }
   
   return (
-    <div className="grid-container">
-      <head>
-        <link rel="stylesheet" href="style.css" />
-        <title>Jet Piranha</title>
-      </head>
-      <body>
-        <div>
-          <header className="header">
-            <div className="brand">
-              <button onClick = {openMenu}>&#9776; </button>
-              <a href="index.html">Jet Piranha</a>
-            </div> 
-            <div className="header-links">
-              <a href="cart.html">Cart</a>
-              <a href="signin.html">Sign In</a>
-            </div>
-          </header>
-          <aside className = "sidebar">
-            <h3> Shopping Categories </h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-            <ul>
-              <li>
-                <a href="index.html">Pants</a>
-              </li>
+    <Router>
+      <div className="grid-container">
+        <head>
+          <link rel="stylesheet" href="style.css" />
+          <title>Jet Piranha</title>
+        </head>
+        <body>
+          <div>
+            <header className="header">
+              <div className="brand">
+                <button onClick = {openMenu}>&#9776; </button>
+                <a href="index.html">Jet Piranha</a>
+              </div> 
+              <div className="header-links">
+                <Link to="/">Home</Link>
+                <Link to="/catalog">Catalog</Link>
+              </div>
+            </header>
+            <aside className = "sidebar">
+              <h3> Shopping Categories </h3>
+              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+              <ul>
+                <li>
+                  <a href="index.html">Pants</a>
+                </li>
 
-              <li>
-                <a href="index.html">Shirts</a>
-              </li>
-            </ul>
-          </aside>
-          <main className="main">
-            <div className="content">
-              <ul className="products">
                 <li>
-                  <div className="product">
-                    <img className="product-image" src="d1.jpg" alt="product"/>
-                    <div className="product-name">
-                      <a href="product.html">Slim Shirts</a>
-                    </div>
-                    <div className="product-brand">Nike</div>
-                    <div className="product-price">$60</div>
-                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="product">
-                      <img className="product-image" src="d1.jpg" alt="product"/>
-                      <div className="product-name">
-                          <a href="product.html">Slim Shirts</a>
-                      </div>
-                      <div className="product-brand">Nike</div>
-                      <div className="product-price">$60</div>
-                      <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                  </div>
-                </li>
-                <li>
-                  <div className="product">
-                    <img className="product-image" src="d1.jpg" alt="product"/>
-                    <div className="product-name">
-                      <a href="product.html">Slim Shirts</a>
-                    </div>
-                    <div className="product-brand">Nike</div>
-                    <div className="product-price">$60</div>
-                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                  </div>
+                  <a href="index.html">Shirts</a>
                 </li>
               </ul>
-            </div>
-          </main>
-          <footer className="footer">
-            <a href="index.html">&copy; 2021 Jet Piranha</a>
-          </footer>
-        </div>
-      </body>
-    </div>
+            </aside>
+            <main className="main">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/catalog">
+                  <Product />
+                </Route>
+              </Switch>
+            </main>
+            <footer className="footer">
+              <a href="index.html">&copy; 2021 Jet Piranha</a>
+            </footer>
+          </div>
+        </body>
+      </div>
+    </Router>
   );
 }
 
